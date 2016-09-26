1bdb31e83c9bcfbd0ebcf49cbbb8cc27edf1a618
Jahseem Maxwell <jahseemmaxwell@uagateway.org>
2016-09-26 16:03:48 +0000

    initial commit

---
+++ Unit1/JMaxwell_calcCtoF.py
@@ -0,0 +1,7 @@
+C = input("Enter a temperature for Celcius! ")
+
+print ( str(C) + " C is equal to " + str(float(C) * 1.8 + 32) + " F.")
+
+#F = input("Enter a temperature for Farenheit! ")
+
+#print ( str(F) + " F is equal to " + str((float(F) -32) / 1.8) + " C.")
---
+++ Unit1/JMaxwell_calcFtoC.py
@@ -0,0 +1,3 @@
+F = input("Enter a temperature for Farenheit! ")
+
+print ( str(F) + " F is equal to " + str((float(F) -32) / 1.8) + " C.")
