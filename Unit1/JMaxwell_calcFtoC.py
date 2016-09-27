59fa11fa23d944eda5cb2f903b4bdc2f9ea2a65e
Jahseem Maxwell <jahseemmaxwell@uagateway.org>
2016-09-27 15:46:05 +0000

    initial commit

--- Unit1/JMaxwell_calcFtoC.py
+++ Unit1/JMaxwell_calcFtoC.py
@@ -1,3 +1,7 @@
 F = input("Enter a temperature for Farenheit! ")
 
 print ( str(F) + " F is equal to " + str((float(F) -32) / 1.8) + " C.")
+
+#C = input("Enter a temperature for Celcius! ")
+
+#print ( str(C) + " C is equal to " + str(float(C) * 1.8 + 32) + " F.")
---
+++ Unit1/JMaxwell_seconds.py
@@ -0,0 +1,7 @@
+second = input("Please enter a number of seconds: ")
+second = int(second)
+min = second / 60
+sec = second % 60
+hour = min / 60
+min = min % 60
+print ("This is " + str(hour) + " hours " + str(min) + " minutes and " + str(sec) + " seconds.")
