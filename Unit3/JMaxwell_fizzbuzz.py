7ef3915dd2bf4608133828842d9df218e610fcaf
Jahseem Maxwell <jahseemmaxwell@uagateway.org>
2016-10-27 15:51:36 +0000

    Finished Lab 3 - 1

--- Unit3/JMaxwell_fizzbuzz.py
+++ Unit3/JMaxwell_fizzbuzz.py
@@ -1,20 +1,13 @@
-# num = int(input("Enter a number after 15: "))
-# for x in range(1,num+1):
-    
-#     print(x)
-# print(num)
-# if 
+swaggy = int(input("Enter a number to start at: "))
+P = int(input("Enter a number to end at: "))
 
-
-
-i = input("Enter any number after 15: ")
-n = int(i)
-for x in range(1,n):
-    if x % 3 == 0:
+for x in range(swaggy - 1, P):
+    x = x + 1
+    if x % 3 == 0 and x % 5 == 0:
+        print ("fizzbuzz")
+    elif x % 3 == 0:
         print ("fizz")
     elif x % 5 == 0:
         print ("buzz")
-    elif x % 5 == 0 and x % 3 == 0:
-        print ("fizzbuzz")
-    print (x)
-    
+    else:
+        print (x)
