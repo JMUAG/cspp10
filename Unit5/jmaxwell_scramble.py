1ef04143fc0594b2e7a2f21082d4cef1b2c19dbe
Jahseem Maxwell <jahseemmaxwell@uagateway.org>
2017-02-02 17:13:41 +0000

    WOrking on SUdo code

--- Unit5/jmaxwell_scramble.py
+++ Unit5/jmaxwell_scramble.py
@@ -5,6 +5,10 @@ import random
 #   arguments: victim - the word that is being scrambled
 #   returns: scrambled word
 
+# 1st - You print the regular word
+# 2nd - You find out if the regular word has more than 3 letters
+# 3rd - If it doesn't, tell the user in put in a different word
+# 4th - If it does, then turn the 
 def scramble_word(victim):
     print(victim)
     if len(victim) > 3:
