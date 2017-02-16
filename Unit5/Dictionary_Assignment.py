108f49222aed28e81982564bc149890afb1a2240
Jahseem Maxwell <jahseemmaxwell@uagateway.org>
2017-02-16 17:15:41 +0000

    Almost done with the project

--- Unit5/Dictionary_Assignment.py
+++ Unit5/Dictionary_Assignment.py
@@ -5,18 +5,22 @@ program = {
 }
 
 def add(program):
-    ask_user = input("Would you like to add or remove to the dictionaries? ").lower()
-    if ask_user == "add":
-        add_user = input("What would you like to be your key? ")
-        add_user2 = input("WHat would you like to be your value? ")
-        program[add_user] = add_user2
-        print(program[add_user])
+    add_user = input("What would you like to be your key? ")
+    add_user2 = input("What would you like to be your value? ")
+    program[add_user] = add_user2
+    
 
-add(program)
+def remove(program):
+        remove_user = input("What would you like to remove? ")
+        if remove_user in program:
+            del [remove_user]
+            
+def update(program):
+    update_user = input("Which Key would you like to modify? ")
+    if update_user in program:
+        update_key = input("What is the new key? ")
+        update_value = input("What is the new value? ")
+        program[update_key] = update_value
         
-    # elif ask_user == "remove":
-    #     remove_user = input("What would you like to remove?")
-    #     if remove_user in program:
-    #          program = {
-                 
-    #          }   
+add(program)
+
